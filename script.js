cb814e4dc36d2db6216b407b5a6b340b5b229804
CNZEN APPS & GAMES <randompupil2008@gmail.com>
2025-08-25 23:18:49 +0300

    Update script.js
    tedt

--- script.js
+++ script.js
@@ -55,6 +55,9 @@ var blocks = setInterval(function(){
         game.appendChild(hole);
         currentBlocks.push(counter);
         counter++;
+currentBlocks.push(counter);
+counter++;
+document.getElementById("score").innerText = "Score: " + (counter - 9);
     }
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
